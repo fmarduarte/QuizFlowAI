a7c17449301fdf6e464318ed68162fbd839210a6
fmarduarte <fabio.multicom@gmail.com>
2026-05-28 01:16:30 -0300

    Convert to pure Vite React

---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+    return (
+      <div
+        style={{
+          minHeight: "100vh",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "48px",
+          fontWeight: "bold",
+        }}
+      >
+        QUIZFLOW WORKING
+      </div>
+    )
+  }
